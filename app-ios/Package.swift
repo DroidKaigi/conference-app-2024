7a7c7a2cde0c083ae9e8cb3d0b7d512afaa3dcd0
takahirom <takam.dev@gmail.com>
2024-04-20 19:03:55 +0900

    Use debug framework for debugging iOS KMP

--- app-ios/Package.swift
+++ app-ios/Package.swift
@@ -25,7 +25,11 @@ let package = Package(
             name: "AppTests",
             dependencies: ["App"]
         ),
+        // For debug
+        // Please run ./gradlew app-ios-shared:linkDebugFrameworkIosSimulatorArm64 first
+        .binaryTarget(name: "KmpModule", path: "../app-ios-shared/build/bin/iosSimulatorArm64/debugFramework/shared.framework"),
+        // For release
         // Please run ./gradlew app-ios-shared:assembleSharedXCFramework first
-        .binaryTarget(name: "KmpModule", path: "../app-ios-shared/build/XCFrameworks/release/shared.xcframework"),
+        //   .binaryTarget(name: "KmpModule", path: "../app-ios-shared/build/XCFrameworks/release/shared.xcframework"),
     ]
 )
