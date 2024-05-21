555bad07b334cce8f9bb84fd5a285d834157f500
ryoya ito <ry.itto.github@gmail.com>
2024-05-21 11:04:17 +0900

    update ci.sh for ios build

--- ci.sh
+++ ci.sh
@@ -3,5 +3,13 @@
 ./gradlew assembleSharedXCFramework --no-configuration-cache
 ./gradlew iosSimulatorArm64Test --no-configuration-cache
 
+# iOS build (Xcode 15.3)
+cd app-ios
+PROJECT=App/App.xcodeproj
+PLATFORM_IOS="iOS Simulator,name=iPhone 15 Pro,OS=17.4"
 
-
+set -o pipefail && xcodebuild build \
+-project $PROJECT \
+-scheme "DroidKaigi2024AppWithKmpBuild" \
+-configuration Debug \
+-destination platform="$PLATFORM_IOS"
