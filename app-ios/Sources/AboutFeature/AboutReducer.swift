425d67ace13b7addf318a5fe92726c24d0d50e18
MrSmart00 <nexearth@gmail.com>
2024-06-14 20:37:06 +0900

    Add: NavigationStack with TCA

--- app-ios/Sources/AboutFeature/AboutReducer.swift
+++ app-ios/Sources/AboutFeature/AboutReducer.swift
@@ -6,20 +6,54 @@ public struct AboutReducer {
     
     @ObservableState
     public struct State: Equatable {
-        var text: String
+        var path = StackState<Path.State>()
+        
+        public init(path: StackState<Path.State> = .init()) {
+          self.path = path
+        }
     }
 
     public enum Action {
-        case onAppear
+        case path(StackAction<Path.State, Path.Action>)
+        case view(View)
+        
+        @CasePathable
+        public enum View {
+            case staffsTapped
+            case contributersTapped
+            case sponsorsTapped
+            case codeOfConductTapped
+            case acknowledgementsTapped
+            case privacyPolicyTapped
+        }
+    }
+
+    @Reducer(state: .equatable)
+    public enum Path {
+        case staffs
+        case contributers
+        case sponsors
+        case acknowledgements
     }
 
     public var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
-            case .onAppear:
-                state.text = "About Feature"
+            case .view(.staffsTapped):
+                state.path.append(.staffs)
+                return .none
+            case .view(.contributersTapped):
+                state.path.append(.contributers)
+                return .none
+            case .view(.sponsorsTapped):
+                state.path.append(.sponsors)
+                return .none
+            case .view:
+                return .none
+            case .path:
                 return .none
             }
         }
+        .forEach(\.path, action: \.path)
     }
 }
--- app-ios/Sources/AboutFeature/AboutView.swift
+++ app-ios/Sources/AboutFeature/AboutView.swift
@@ -2,22 +2,85 @@ import ComposableArchitecture
 import SwiftUI
 
 public struct AboutView: View {
-    private let store: StoreOf<AboutReducer>
+    @Bindable var store: StoreOf<AboutReducer>
 
     public init(store: StoreOf<AboutReducer>) {
         self.store = store
     }
 
     public var body: some View {
-        VStack {
-            KeyVisual()
-                .padding(16)
-            Spacer()
+        NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
+            ScrollView {
+                VStack(spacing: 0) {
+                    KeyVisual()
+                        .padding(.top, 28)
+                        .padding(.bottom, 32)
+                    
+                    VStack {
+                        HStack {
+                            Text("Credits")
+                                .foregroundStyle(Color(.surfaceOnSurfaceVariant))
+                                .font(.headline)
+                            Spacer()
+                        }
+
+                        Button(action: {
+                            store.send(.view(.staffsTapped))
+                        }, label: {
+                            Label("Staffs", systemImage: "face.smiling")
+                                .foregroundStyle(Color(.surfaceOnSurface))
+                            Spacer()
+                        })
+                        .padding(.init(top: 24, leading: 14, bottom: 24, trailing: 14))
+                        
+                        Divider()
+                            .background(Color(.outlineOutlineVariant))
+
+                        Button(action: {
+                            store.send(.view(.contributersTapped))
+                        }, label: {
+                            Label("Contributers", systemImage: "person.3")
+                                .foregroundStyle(Color(.surfaceOnSurface))
+                            Spacer()
+                        })
+                        .padding(.init(top: 24, leading: 14, bottom: 24, trailing: 14))
+
+                        Divider()
+                            .background(Color(.outlineOutlineVariant))
+
+                        Button(action: {
+                            store.send(.view(.sponsorsTapped))
+                        }, label: {
+                            Label("Sponsors", systemImage: "building.2")
+                                .foregroundStyle(Color(.surfaceOnSurface))
+                            Spacer()
+                        })
+                        .padding(.init(top: 24, leading: 14, bottom: 24, trailing: 14))
+
+                        Divider()
+                            .background(Color(.outlineOutlineVariant))
+
+                    }
+                    
+                }
+                .padding(.horizontal, 16)
+            }
+            .background(Color(.background))
+        } destination: { store in
+            switch store.state {
+            case .staffs:
+                Text("Staff")
+            case .contributers:
+                Text("Contributers")
+            case .sponsors:
+                Text("Sponsors")
+            case .acknowledgements:
+                Text("Acknowledgements")
+            }
         }
-        .background(Color(.background))
     }
 }
 
 #Preview {
-    AboutView(store: .init(initialState: .init(text: "Hoge"), reducer: { AboutReducer() }))
+    AboutView(store: .init(initialState: .init(), reducer: { AboutReducer() }))
 }
