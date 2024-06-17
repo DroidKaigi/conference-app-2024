3db64ce23494f1f41805845387505f46daee594b
MrSmart00 <nexearth@gmail.com>
2024-06-17 23:22:40 +0900

    Fix: update unittest

--- app-ios/Tests/AboutFeatureTests/AboutFeatureTests.swift
+++ app-ios/Tests/AboutFeatureTests/AboutFeatureTests.swift
@@ -34,4 +34,34 @@ final class AboutFeatureTests: XCTestCase {
             $0.path[id: 0] = .sponsors
         }
     }
+    
+    @MainActor
+    func testTappedCodeOfConduct() async {
+        let store = TestStore(initialState: AboutReducer.State()) {
+            AboutReducer()
+        }
+        await store.send(\.view.codeOfConductTapped) {
+            $0.presentation = .codeOfConduct
+        }
+    }
+
+    @MainActor
+    func testTappedAcknowledgements() async {
+        let store = TestStore(initialState: AboutReducer.State()) {
+            AboutReducer()
+        }
+        await store.send(\.view.acknowledgementsTapped) {
+            $0.path[id: 0] = .acknowledgements
+        }
+    }
+
+    @MainActor
+    func testTappedPrivacyPolicy() async {
+        let store = TestStore(initialState: AboutReducer.State()) {
+            AboutReducer()
+        }
+        await store.send(\.view.privacyPolicyTapped) {
+            $0.presentation = .privacyPolicy
+        }
+    }
 }
