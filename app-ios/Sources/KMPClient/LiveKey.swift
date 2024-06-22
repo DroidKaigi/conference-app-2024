9db8a088f007ff3c074167b394cd936df34deaeb
Ryoya Ito <ry.itto.github@gmail.com>
2024-06-22 23:23:01 +0900

    add dependency clients for kmp (#33)

--- app-ios/Sources/KMPClient/LiveKey.swift
+++ app-ios/Sources/KMPClient/LiveKey.swift
@@ -5,10 +5,46 @@ private var sessionsRepository: any SessionsRepository {
     Container.shared.get(type: (any SessionsRepository).self)
 }
 
-extension KMPClient: DependencyKey {
-    public static let liveValue: KMPClient = .init(
-        fetchTimetable: {
+private var staffRepository: any StaffRepository {
+    Container.shared.get(type: (any StaffRepository).self)
+}
+
+private var sponsorsRepository: any SponsorsRepository {
+    Container.shared.get(type: (any SponsorsRepository).self)
+}
+
+extension TimetableClient: DependencyKey {
+    public static let liveValue: TimetableClient = .init(
+        streamTimetable: {
             sessionsRepository.getTimetableStream().eraseToThrowingStream()
+        },
+        streamTimetableItemWithFavorite: { id in
+            sessionsRepository
+                .getTimetableItemWithBookmarkStream(id: id)
+                .compactMap { pair in
+                    guard let item = pair.first, let isFavorited = pair.second?.boolValue else { return nil }
+                    return (item, isFavorited)
+                }
+                .eraseToThrowingStream()
+        },
+        toggleBookmark: { id in
+            try await sessionsRepository.toggleBookmark(id: id)
+        }
+    )
+}
+
+extension StaffClient: DependencyKey {
+    public static let liveValue: StaffClient = .init(
+        streamStaffs: {
+            staffRepository.staffs().eraseToThrowingStream()
+        }
+    )
+}
+
+extension SponsorsClient: DependencyKey {
+    public static let liveValue: SponsorsClient = .init(
+        streamSponsors: {
+            sponsorsRepository.sponsors().eraseToThrowingStream()
         }
     )
 }
