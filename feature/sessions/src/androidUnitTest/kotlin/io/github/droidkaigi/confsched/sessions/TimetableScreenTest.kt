e631ac904e4ff86d75912ddac97d2e4fdaa34a0a
takahirom <takam.dev@gmail.com>
2024-06-25 11:51:50 +0900

    Add describe based Robolectric test

--- feature/sessions/src/androidUnitTest/kotlin/io/github/droidkaigi/confsched/sessions/TimetableScreenTest.kt
+++ feature/sessions/src/androidUnitTest/kotlin/io/github/droidkaigi/confsched/sessions/TimetableScreenTest.kt
@@ -1,22 +1,23 @@
 package io.github.droidkaigi.confsched.sessions
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidTest
+import io.github.droidkaigi.confsched.testing.DescribedTestCase
 import io.github.droidkaigi.confsched.testing.RobotTestRule
 import io.github.droidkaigi.confsched.testing.TimetableServerRobot.ServerStatus
+import io.github.droidkaigi.confsched.testing.describeTests
+import io.github.droidkaigi.confsched.testing.execute
 import io.github.droidkaigi.confsched.testing.robot.TimetableScreenRobot
 import io.github.droidkaigi.confsched.testing.runRobot
-import io.github.droidkaigi.confsched.testing.todoChecks
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
+import org.robolectric.ParameterizedRobolectricTestRunner
 import javax.inject.Inject
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(ParameterizedRobolectricTestRunner::class)
 @HiltAndroidTest
-class TimetableScreenTest {
+class TimetableScreenTest(private val testCase: DescribedTestCase<TimetableScreenRobot>) {
 
     @get:Rule
     @BindValue val robotTestRule: RobotTestRule = RobotTestRule(this)
@@ -25,101 +26,68 @@ class TimetableScreenTest {
     lateinit var timetableScreenRobot: TimetableScreenRobot
 
     @Test
-    fun checkLaunchShot() {
+    fun runTest() {
         runRobot(timetableScreenRobot) {
-            setupTimetableScreenContent()
-            captureScreenWithChecks(checks = todoChecks("TODO: Please add some checks!"))
+            testCase.execute(timetableScreenRobot)
         }
     }
 
-    @Test
-    fun checkLaunchServerErrorShot() {
-        runRobot(timetableScreenRobot) {
-            setupTimetableServer(ServerStatus.Error)
-            setupTimetableScreenContent()
-            captureScreenWithChecks(checks = todoChecks("TODO: Please add some checks!"))
-        }
-    }
-
-    @Test
-    fun checkLaunch() {
-        runRobot(timetableScreenRobot) {
-            setupTimetableScreenContent()
-            checkTimetableItemsDisplayed()
-        }
-    }
-
-    @Test
-    fun checkLaunchAccessibilityShot() {
-        runRobot(timetableScreenRobot) {
-            setupTimetableScreenContent()
-            checkAccessibilityCapture()
-        }
-    }
-
-    @Test
-    fun checkBookmarkToggleShot() {
-        runRobot(timetableScreenRobot) {
-            setupTimetableScreenContent()
-            clickFirstSessionBookmark()
-            captureScreenWithChecks()
-            clickFirstSessionBookmark()
-            captureScreenWithChecks()
-        }
-    }
-
-    @Test
-    fun checkScrollShot() {
-        runRobot(timetableScreenRobot) {
-            setupTimetableScreenContent()
-            scrollTimetable()
-            captureScreenWithChecks()
-        }
-    }
-
-    @Test
-    fun checkGridShot() {
-        runRobot(timetableScreenRobot) {
-            setupTimetableScreenContent()
-            clickTimetableUiTypeChangeButton()
-            captureScreenWithChecks()
-        }
-    }
-
-    @Test
-    fun checkGridScrollShot() {
-        runRobot(timetableScreenRobot) {
-            setupTimetableScreenContent()
-            clickTimetableUiTypeChangeButton()
-            scrollTimetable()
-            captureScreenWithChecks()
-        }
-    }
-
-    @Test
-    @Config(fontScale = 0.5f)
-    fun checkSmallFontScaleShot() {
-        runRobot(timetableScreenRobot) {
-            setupTimetableScreenContent()
-            captureScreenWithChecks(checks = todoChecks("TODO: Please add some checks!"))
-        }
-    }
-
-    @Test
-    @Config(fontScale = 1.5f)
-    fun checkLargeFontScaleShot() {
-        runRobot(timetableScreenRobot) {
-            setupTimetableScreenContent()
-            captureScreenWithChecks(checks = todoChecks("TODO: Please add some checks!"))
-        }
-    }
-
-    @Test
-    @Config(fontScale = 2.0f)
-    fun checkHugeFontScaleShot() {
-        runRobot(timetableScreenRobot) {
-            setupTimetableScreenContent()
-            captureScreenWithChecks(checks = todoChecks("TODO: Please add some checks!"))
+    companion object {
+        @JvmStatic
+        @ParameterizedRobolectricTestRunner.Parameters(name = "{0}")
+        fun tests(): List<DescribedTestCase<TimetableScreenRobot>> {
+            return describeTests<TimetableScreenRobot> {
+                describe("TimetableScreenTest") {
+                    describe("when server is operational") {
+                        run { robot ->
+                            robot.setupTimetableServer(ServerStatus.Operational)
+                            robot.setupTimetableScreenContent()
+                        }
+                        check("should show timetable items") { robot ->
+                            robot.captureScreenWithChecks(checks = {
+                                robot.checkTimetableItemsDisplayed()
+                            })
+                        }
+                        describe("click first session bookmark") {
+                            run { robot ->
+                                robot.clickFirstSessionBookmark()
+                            }
+                            check("should show bookmarked session") { robot ->
+                                // FIXME: Add check for bookmarked session
+                                robot.captureScreenWithChecks()
+                            }
+                        }
+                        describe("click first session") {
+                            run { robot ->
+                                robot.clickFirstSession()
+                            }
+                            check("should show session detail") { robot ->
+                                // FIXME: Add check for navigation to session detail
+                                robot.captureScreenWithChecks()
+                            }
+                        }
+                        describe("click timetable ui type change button") {
+                            run { robot ->
+                                robot.clickTimetableUiTypeChangeButton()
+                            }
+                            check("should change timetable ui type") { robot ->
+                                // FIXME: Add check for timetable ui type change
+                                robot.captureScreenWithChecks()
+                            }
+                        }
+                    }
+                    describe("when server is down") {
+                        run { robot ->
+                            robot.setupTimetableServer(ServerStatus.Error)
+                            robot.setupTimetableScreenContent()
+                        }
+                        check("should show error message") { robot ->
+                            // FIXME: Add check for error message
+                            robot.captureScreenWithChecks()
+                        }
+                    }
+                }
+            }
         }
     }
 }
