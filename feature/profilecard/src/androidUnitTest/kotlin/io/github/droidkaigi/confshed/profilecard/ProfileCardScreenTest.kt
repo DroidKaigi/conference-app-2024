13b83e33befe09476f2ba5383d446d73001e70ea
Ichinose Yoshihiro <maruisansmai@gmail.com>
2024-07-10 02:12:59 +0900

    test: change behavior spec

--- feature/profilecard/src/androidUnitTest/kotlin/io/github/droidkaigi/confshed/profilecard/ProfileCardScreenTest.kt
+++ feature/profilecard/src/androidUnitTest/kotlin/io/github/droidkaigi/confshed/profilecard/ProfileCardScreenTest.kt
@@ -1,19 +1,24 @@
 package io.github.droidkaigi.confshed.profilecard
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidTest
+import io.github.droidkaigi.confsched.testing.DescribedBehavior
 import io.github.droidkaigi.confsched.testing.RobotTestRule
+import io.github.droidkaigi.confsched.testing.describeBehaviors
+import io.github.droidkaigi.confsched.testing.execute
 import io.github.droidkaigi.confsched.testing.robot.ProfileCardScreenRobot
 import io.github.droidkaigi.confsched.testing.runRobot
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
 import javax.inject.Inject
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(ParameterizedRobolectricTestRunner::class)
 @HiltAndroidTest
-class ProfileCardScreenTest {
+class ProfileCardScreenTest(
+    private val testCase: DescribedBehavior<ProfileCardScreenRobot>,
+) {
     @get:Rule
     @BindValue
     val robotTestRule: RobotTestRule = RobotTestRule(this)
@@ -22,22 +27,25 @@ class ProfileCardScreenTest {
     lateinit var robot: ProfileCardScreenRobot
 
     @Test
-    fun showEditScreen() {
+    fun runTest() {
         runRobot(robot) {
-            setupScreenContent()
-            captureScreenWithChecks {
-                checkEditScreenDisplayed()
-            }
+            testCase.execute(robot)
         }
     }
 
-    @Test
-    fun showCardScreen() {
-        runRobot(robot) {
-            setupScreenContent()
-            captureScreenWithChecks {
-                // TODO: need fake repository
-                // checkCardScreenDisplayed()
+    companion object {
+        @JvmStatic
+        @ParameterizedRobolectricTestRunner.Parameters(name = "{0}")
+        fun behaviors(): List<DescribedBehavior<ProfileCardScreenRobot>> {
+            return describeBehaviors("ProfileCardScreen") {
+                describe("when profile card is not found") {
+                    run {
+                        setupScreenContent()
+                    }
+                    itShould("show edit screen") {
+                        checkEditScreenDisplayed()
+                    }
+                }
             }
         }
     }
