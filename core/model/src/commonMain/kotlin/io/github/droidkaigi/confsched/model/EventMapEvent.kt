7961a9953712ed5f7a44ee652d13ff538f46d3f6
mtkw0127 <maccho.chekera@gmail.com>
2024-07-10 01:49:11 +0900

    Fix typo

--- core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched/model/EventMapEvent.kt
+++ core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched/model/EventMapEvent.kt
@@ -20,7 +20,7 @@ fun createSampleEventMapEvent(
 ) = EventMapEvent(
     name = "ランチミートアップ",
     roomName = "Arctic Fox",
-    dateLabel = "DAT1",
+    dateLabel = "DAY1",
     isFavorite = isFavorite,
     description = "様々なテーマごとに集まって、一緒にランチを食べながらお話ししましょう。席に限りがありますので、お弁当受け取り後お早めにお越しください。",
     startTime = LocalTime(10, 30),
