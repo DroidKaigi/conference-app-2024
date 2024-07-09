58587518b973c11b3460a1a230f086309ef78ebf
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2024-07-09 21:17:28 +0900

    fix: maximize headline area width

--- feature/sessions/src/commonMain/kotlin/io/github/droidkaigi/confsched/sessions/component/TimeTableItemDetailHeadline.kt
+++ feature/sessions/src/commonMain/kotlin/io/github/droidkaigi/confsched/sessions/component/TimeTableItemDetailHeadline.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -37,7 +38,8 @@ fun TimeTableItemDetailHeadline(
         modifier = modifier
             // FIXME: Implement and use a theme color instead of fixed colors like RoomColors.primary and RoomColors.primaryDim
             .background(Color(0xFF132417))
-            .padding(8.dp),
+            .padding(8.dp)
+            .fillMaxWidth(),
     ) {
         Row {
             TagView(
