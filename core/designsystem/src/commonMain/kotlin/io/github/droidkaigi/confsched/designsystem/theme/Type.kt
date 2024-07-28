1d4382a41bec7e3b162e5cb87ce5053238e7853f
takahirom <takam.dev@gmail.com>
2024-07-28 20:53:53 +0900

    Fix format

--- core/designsystem/src/commonMain/kotlin/io/github/droidkaigi/confsched/designsystem/theme/Type.kt
+++ core/designsystem/src/commonMain/kotlin/io/github/droidkaigi/confsched/designsystem/theme/Type.kt
@@ -22,92 +22,92 @@ fun appTypography(): Typography {
                 fontFamily = dotGothic16,
                 fontSize = 57.sp,
                 lineHeight = 64.sp,
-                letterSpacing = (-0.25).sp
+                letterSpacing = (-0.25).sp,
             ),
             displayMedium = TextStyle(
                 fontFamily = dotGothic16,
                 fontSize = 45.sp,
                 lineHeight = 52.sp,
-                letterSpacing = 0.sp
+                letterSpacing = 0.sp,
             ),
             displaySmall = TextStyle(
                 fontFamily = dotGothic16,
                 fontSize = 36.sp,
                 lineHeight = 44.sp,
-                letterSpacing = 0.sp
+                letterSpacing = 0.sp,
             ),
             headlineLarge = TextStyle(
                 fontFamily = dotGothic16,
                 fontSize = 32.sp,
                 lineHeight = 40.sp,
-                letterSpacing = 0.sp
+                letterSpacing = 0.sp,
             ),
             headlineMedium = TextStyle(
                 fontFamily = dotGothic16,
                 fontSize = 28.sp,
                 lineHeight = 36.sp,
-                letterSpacing = 0.sp
+                letterSpacing = 0.sp,
             ),
             headlineSmall = TextStyle(
                 fontFamily = dotGothic16,
                 fontSize = 24.sp,
                 lineHeight = 32.sp,
-                letterSpacing = 0.sp
+                letterSpacing = 0.sp,
             ),
             titleLarge = TextStyle(
                 fontFamily = dotGothic16,
                 fontSize = 22.sp,
                 lineHeight = 28.sp,
-                letterSpacing = 0.sp
+                letterSpacing = 0.sp,
             ),
             titleMedium = TextStyle(
                 fontFamily = dotGothic16,
                 fontSize = 16.sp,
                 lineHeight = 24.sp,
-                letterSpacing = 0.15.sp
+                letterSpacing = 0.15.sp,
             ),
             titleSmall = TextStyle(
                 fontFamily = dotGothic16,
                 fontSize = 14.sp,
                 lineHeight = 20.sp,
-                letterSpacing = 0.1.sp
+                letterSpacing = 0.1.sp,
             ),
             labelLarge = TextStyle(
                 fontFamily = dotGothic16,
                 fontWeight = FontWeight.Medium,
                 fontSize = 14.sp,
                 lineHeight = 20.sp,
-                letterSpacing = 0.1.sp
+                letterSpacing = 0.1.sp,
             ),
             labelMedium = TextStyle(
                 fontFamily = dotGothic16,
                 fontWeight = FontWeight.Medium,
                 fontSize = 12.sp,
                 lineHeight = 16.sp,
-                letterSpacing = 0.5.sp
+                letterSpacing = 0.5.sp,
             ),
             labelSmall = TextStyle(
                 fontFamily = dotGothic16,
                 fontWeight = FontWeight.Medium,
                 fontSize = 11.sp,
                 lineHeight = 16.sp,
-                letterSpacing = 0.5.sp
+                letterSpacing = 0.5.sp,
             ),
             bodyLarge = TextStyle(
                 fontSize = 16.sp,
                 lineHeight = 24.sp,
-                letterSpacing = 0.5.sp
+                letterSpacing = 0.5.sp,
             ),
             bodyMedium = TextStyle(
                 fontSize = 14.sp,
                 lineHeight = 20.sp,
-                letterSpacing = 0.25.sp
+                letterSpacing = 0.25.sp,
             ),
             bodySmall = TextStyle(
                 fontSize = 12.sp,
                 lineHeight = 16.sp,
-                letterSpacing = 0.4.sp
-            )
+                letterSpacing = 0.4.sp,
+            ),
 
         )
     }
