a18658fe58d3eb0a0a1474c0244404944797e4b4
mtkw0127 <maccho.chekera@gmail.com>
2024-08-02 19:10:14 +0900

    Use describeBehaviors

--- feature/contributors/src/androidUnitTest/kotlin/io/github/droidkaigi/confsched/contributors/ContributorsScreenTest.kt
+++ feature/contributors/src/androidUnitTest/kotlin/io/github/droidkaigi/confsched/contributors/ContributorsScreenTest.kt
@@ -6,18 +6,22 @@ import dagger.hilt.android.testing.HiltAndroidTest
 import io.github.droidkaigi.confsched.testing.ContributorsServerRobot
 import io.github.droidkaigi.confsched.testing.DefaultContributorsServerRobot
 import io.github.droidkaigi.confsched.testing.DefaultScreenRobot
+import io.github.droidkaigi.confsched.testing.DescribedBehavior
 import io.github.droidkaigi.confsched.testing.RobotTestRule
 import io.github.droidkaigi.confsched.testing.ScreenRobot
+import io.github.droidkaigi.confsched.testing.describeBehaviors
+import io.github.droidkaigi.confsched.testing.execute
 import io.github.droidkaigi.confsched.testing.runRobot
 import io.github.droidkaigi.confsched.testing.todoChecks
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
 import javax.inject.Inject
 
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
-class ContributorsScreenTest {
+class ContributorsScreenTest(private val testCase: DescribedBehavior<ContributorsScreenRobot>) {
 
     @get:Rule
     @BindValue val robotTestRule: RobotTestRule = RobotTestRule(this)
@@ -26,26 +30,40 @@ class ContributorsScreenTest {
     lateinit var contributorsScreenRobot: ContributorsScreenRobot
 
     @Test
-    fun checkScreenContent() {
+    fun runTest() {
         runRobot(contributorsScreenRobot) {
-            setupContributorServer(ContributorsServerRobot.ServerStatus.Operational)
-            setupScreenContent()
-
-            captureScreenWithChecks(
-                checks = todoChecks("This screen is still empty now. Please add some checks."),
-            )
+            testCase.execute(contributorsScreenRobot)
         }
     }
 
-    @Test
-    fun checkErrorScreenContent() {
-        runRobot(contributorsScreenRobot) {
-            setupContributorServer(ContributorsServerRobot.ServerStatus.Error)
-            setupScreenContent()
-
-            captureScreenWithChecks(
-                checks = todoChecks("This screen is still empty now. Please add some checks."),
-            )
+    companion object {
+        @JvmStatic
+        @ParameterizedRobolectricTestRunner.Parameters(name = "{0}")
+        fun behaviors(): List<DescribedBehavior<ContributorsScreenRobot>> {
+            return describeBehaviors<ContributorsScreenRobot>(name = "ContributorsScreen") {
+                describe("when launch") {
+                    run {
+                        setupContributorServer(ContributorsServerRobot.ServerStatus.Operational)
+                        setupScreenContent()
+                    }
+                    itShould("show contributors list") {
+                        captureScreenWithChecks(
+                            checks = todoChecks("This screen is still empty now. Please add some checks."),
+                        )
+                    }
+                }
+                describe("when launch with error") {
+                    run {
+                        setupContributorServer(ContributorsServerRobot.ServerStatus.Error)
+                        setupScreenContent()
+                    }
+                    itShould("show error message") {
+                        captureScreenWithChecks(
+                            checks = todoChecks("This screen is still empty now. Please add some checks."),
+                        )
+                    }
+                }
+            }
         }
     }
 }
