8ed59fc16fbc8526d4a76da29b4a44dbb85415f4
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2024-08-05 23:39:43 +0900

    chore: add dependency from core:ui to core:model

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -13,6 +13,7 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
+                implementation(projects.core.model)
                 implementation(projects.core.designsystem)
                 implementation(projects.core.data)
                 implementation(libs.kermit)
