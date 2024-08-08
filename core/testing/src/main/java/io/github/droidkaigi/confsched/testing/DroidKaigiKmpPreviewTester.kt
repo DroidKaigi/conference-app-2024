0a15d6b930e981217645ecd0d1be5bde4d412f22
takahirom <takam.dev@gmail.com>
2024-08-08 14:12:52 +0900

    Fix import

--- core/testing/src/main/java/io/github/droidkaigi/confsched/testing/DroidKaigiKmpPreviewTester.kt
+++ core/testing/src/main/java/io/github/droidkaigi/confsched/testing/DroidKaigiKmpPreviewTester.kt
@@ -3,12 +3,14 @@ package io.github.droidkaigi.confsched.testing
 import com.github.takahirom.roborazzi.ComposePreviewTester
 import com.github.takahirom.roborazzi.ComposePreviewTester.Options
 import com.github.takahirom.roborazzi.ComposePreviewTester.Options.JUnit4TestLifecycleOptions
+import com.github.takahirom.roborazzi.ExperimentalRoborazziApi
 import com.github.takahirom.roborazzi.captureRoboImage
 import io.github.droidkaigi.confsched.testing.rules.CoilRule
 import sergio.sastre.composable.preview.scanner.core.preview.ComposablePreview
 import sergio.sastre.composable.preview.scanner.jvm.JvmAnnotationScanner
 import sergio.sastre.composable.preview.scanner.jvm.JvmAnnotationScanner.DesktopPreviewInfo
 
+@OptIn(ExperimentalRoborazziApi::class)
 class DroidKaigiKmpPreviewTester : ComposePreviewTester<JvmAnnotationScanner.DesktopPreviewInfo> {
     override fun options(): Options {
         return super.options().copy(
--- core/testing/src/main/java/io/github/droidkaigi/confsched/testing/robot/StaffScreenRobot.kt
+++ core/testing/src/main/java/io/github/droidkaigi/confsched/testing/robot/StaffScreenRobot.kt
@@ -2,10 +2,6 @@ package io.github.droidkaigi.confsched.testing.robot
 
 import io.github.droidkaigi.confsched.designsystem.theme.KaigiTheme
 import io.github.droidkaigi.confsched.staff.StaffScreen
-import io.github.droidkaigi.confsched.testing.DefaultScreenRobot
-import io.github.droidkaigi.confsched.testing.DefaultStaffServerRobot
-import io.github.droidkaigi.confsched.testing.ScreenRobot
-import io.github.droidkaigi.confsched.testing.StaffServerRobot
 import javax.inject.Inject
 
 class StaffScreenRobot @Inject constructor(
