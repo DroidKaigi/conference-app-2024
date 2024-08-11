da4d02440bc5d06da338dced7343190e3653e4c0
sanao1006 <naosak1006@gmail.com>
2024-08-11 19:26:19 +0900

    fix jaTitle

--- core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched/model/EventMapEvent.kt
+++ core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched/model/EventMapEvent.kt
@@ -8,7 +8,7 @@ data class EventMapEvent(
 
 fun createSampleEventMapEvent() = EventMapEvent(
     name = MultiLangText("ランチミートアップ", "Lunch Meetup"),
-    roomName = MultiLangText("イグアナ", "Iguana"),
+    roomName = MultiLangText("Iguana", "Iguana"),
     description = MultiLangText(
         "様々なテーマごとに集まって、一緒にランチを食べながらお話ししましょう。席に限りがありますので、お弁当受け取り後お早めにお越しください。",
         "Let's gather for lunch and chat about various topics. Seats are limited, so please come soon after receiving your lunch box.",
