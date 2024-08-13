d23e22cfe312e6cc1e3a52aca9914ae1ec659bda
gibachan <childhoodend.tk@gmail.com>
2024-08-13 10:14:32 +0900

    Fix to allow CompositionLocals

--- core/ui/src/commonMain/kotlin/io/github/droidkaigi/confsched/ui/compositionlocal/LocalAnimatedVisibilityScope.kt
+++ core/ui/src/commonMain/kotlin/io/github/droidkaigi/confsched/ui/compositionlocal/LocalAnimatedVisibilityScope.kt
@@ -3,4 +3,5 @@ package io.github.droidkaigi.confsched.ui.compositionlocal
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.runtime.compositionLocalOf
 
+@Suppress("CompositionLocalAllowlist")
 val LocalAnimatedVisibilityScope = compositionLocalOf<AnimatedVisibilityScope?> { null }
--- core/ui/src/commonMain/kotlin/io/github/droidkaigi/confsched/ui/compositionlocal/LocalSharedTransitionScope.kt
+++ core/ui/src/commonMain/kotlin/io/github/droidkaigi/confsched/ui/compositionlocal/LocalSharedTransitionScope.kt
@@ -4,5 +4,6 @@ import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.runtime.compositionLocalOf
 
+@Suppress("CompositionLocalAllowlist")
 @OptIn(ExperimentalSharedTransitionApi::class)
 val LocalSharedTransitionScope = compositionLocalOf<SharedTransitionScope?> { null }
