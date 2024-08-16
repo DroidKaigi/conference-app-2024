f2175d1d691f79f846f29931ff12036fc528b1eb
kosenda <kkkk0630@ezweb.ne.jp>
2024-08-16 10:15:36 +0900

    feature: add animations in TimetableItemDetailScreen

--- core/designsystem/src/commonMain/kotlin/io/github/droidkaigi/confsched/designsystem/component/ClickableLinkText.kt
+++ core/designsystem/src/commonMain/kotlin/io/github/droidkaigi/confsched/designsystem/component/ClickableLinkText.kt
@@ -1,5 +1,7 @@
 package io.github.droidkaigi.confsched.designsystem.component
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -19,6 +21,8 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
+
+private const val ClickableTextExpandAnimateDurationMillis = 1000
 
 @Composable
 private fun findResults(
@@ -118,9 +122,11 @@ fun ClickableLinkText(
     }
 
     ClickableText(
-        modifier = modifier.onGloballyPositioned { coordinates ->
-            layoutResult.value = coordinates
-        },
+        modifier = modifier
+            .animateContentSize(animationSpec = tween(ClickableTextExpandAnimateDurationMillis))
+            .onGloballyPositioned { coordinates ->
+                layoutResult.value = coordinates
+            },
         text = annotatedString,
         style = style,
         overflow = overflow,
