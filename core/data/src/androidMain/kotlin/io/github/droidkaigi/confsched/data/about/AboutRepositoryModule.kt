786f35b625fedfd33c7f3059af8090f493808bb4
obaya884 <taku3.884@gmail.com>
2024-08-16 14:52:32 +0900

    get app version from build config

---
+++ core/data/src/androidMain/kotlin/io/github/droidkaigi/confsched/data/about/AboutRepositoryModule.kt
@@ -0,0 +1,35 @@
+package io.github.droidkaigi.confsched.data.about
+
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dagger.multibindings.ClassKey
+import dagger.multibindings.IntoMap
+import io.github.droidkaigi.confsched.data.di.RepositoryQualifier
+import io.github.droidkaigi.confsched.model.AboutRepository
+import io.github.droidkaigi.confsched.model.BuildConfigProvider
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+public abstract class AboutRepositoryModule {
+    @Binds
+    @RepositoryQualifier
+    @IntoMap
+    @ClassKey(AboutRepository::class)
+    public abstract fun bind(repository: DefaultAboutRepository): Any
+
+    public companion object {
+        @Provides
+        @Singleton
+        public fun provideAboutRepository(
+            buildConfigProvider: BuildConfigProvider,
+        ): AboutRepository {
+            return DefaultAboutRepository(
+                buildConfigProvider = buildConfigProvider,
+            )
+        }
+    }
+}
