331327c4a8547b21e4b5a070ef10eb3077df02d8
nyafunta9858 <kebottoandroider.10@gmail.com>
2024-08-16 15:08:57 +0900

    fix detekt

--- core/designsystem/src/desktopMain/kotlin/io/github/droidkaigi/confsched/designsystem/theme/Type.desktop.kt
+++ core/designsystem/src/desktopMain/kotlin/io/github/droidkaigi/confsched/designsystem/theme/Type.desktop.kt
@@ -2,12 +2,9 @@ package io.github.droidkaigi.confsched.designsystem.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.platform.Typeface
 import conference_app_2024.core.designsystem.generated.resources.dot_gothic16_regular
 import io.github.droidkaigi.confsched.designsystem.DesignSystemRes
 import org.jetbrains.compose.resources.Font
-import org.jetbrains.skia.FontStyle
-import org.jetbrains.skia.Typeface
 
 @Composable
 actual fun dotGothic16FontFamily(): FontFamily = FontFamily(
