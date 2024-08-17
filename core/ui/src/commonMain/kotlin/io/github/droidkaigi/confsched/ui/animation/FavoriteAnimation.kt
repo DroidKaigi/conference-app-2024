837bd9837c700c4a6d282e2289a5196de673a2ee
shunm-999 <shunmiyazaki7812@gmail.com>
2024-08-17 19:51:41 +0900

    Modified to use drawContent to avoid recomposition

--- core/ui/src/commonMain/kotlin/io/github/droidkaigi/confsched/ui/animation/FavoriteAnimation.kt
+++ core/ui/src/commonMain/kotlin/io/github/droidkaigi/confsched/ui/animation/FavoriteAnimation.kt
@@ -2,24 +2,25 @@ package io.github.droidkaigi.confsched.ui.animation
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.positionInRoot
-import androidx.compose.ui.unit.IntOffset
-import conference_app_2024.core.ui.generated.resources.bookmarked
+import androidx.compose.ui.unit.dp
 import io.github.droidkaigi.confsched.designsystem.theme.primaryFixed
-import io.github.droidkaigi.confsched.ui.UiRes
 import io.github.droidkaigi.confsched.ui.compositionlocal.LocalClock
-import org.jetbrains.compose.resources.stringResource
+import io.github.droidkaigi.confsched.ui.drawInFrontOf
+
+private val IconSize = 24.0.dp
 
 @Composable
 fun ProvideFavoriteAnimation(
@@ -37,42 +38,37 @@ fun ProvideFavoriteAnimation(
         )
     }
 
+    val favoriteIconColor = MaterialTheme.colorScheme.primaryFixed
+    val painter = rememberVectorPainter(Icons.Filled.Favorite)
+
     CompositionLocalProvider(
         LocalFavoriteAnimationScope provides favoriteAnimationScope,
     ) {
-        Box(modifier = modifier.fillMaxSize()) {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .onGloballyPositionedWithFavoriteAnimationScope { scope, coordinates ->
+                    val position = coordinates.positionInRoot()
+                    scope?.setAnimationFramePosition(position)
+                }.drawInFrontOf {
+                    if (favoriteAnimationScope.animations.isNotEmpty()) {
+                        favoriteAnimationScope.animations.forEach {
+                            translate(
+                                left = it.offset.x,
+                                top = it.offset.y,
+                            ) {
+                                with(painter) {
+                                    draw(
+                                        size = Size(IconSize.toPx(), IconSize.toPx()),
+                                        colorFilter = ColorFilter.tint(favoriteIconColor),
+                                    )
+                                }
+                            }
+                        }
+                    }
+                },
+        ) {
             content()
-            FavoriteAnimationFrame()
         }
     }
-}
-
-@Composable
-private fun FavoriteAnimationFrame() {
-    val animationScope = LocalFavoriteAnimationScope.current
-    Box(
-        modifier = Modifier.fillMaxSize()
-            .onGloballyPositionedWithFavoriteAnimationScope { scope, coordinates ->
-                val position = coordinates.positionInRoot()
-                scope?.setAnimationFramePosition(position)
-            },
-    ) {
-        if (animationScope.animations.isNotEmpty()) {
-            animationScope.animations.forEach {
-                Icon(
-                    Icons.Filled.Favorite,
-                    contentDescription = stringResource(UiRes.string.bookmarked),
-                    tint = MaterialTheme.colorScheme.primaryFixed,
-                    modifier = Modifier
-                        .offset {
-                            it.offset.toIntOffset()
-                        },
-                )
-            }
-        }
-    }
-}
-
-private fun Offset.toIntOffset(): IntOffset {
-    return IntOffset(x.toInt(), y.toInt())
 }
