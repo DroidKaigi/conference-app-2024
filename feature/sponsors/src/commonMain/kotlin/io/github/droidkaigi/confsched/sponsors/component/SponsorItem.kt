fd910438b4647f16945ef089f8c6b52224318b8d
Kei-1111 <kei_02_1111@icloud.com>
2024-08-18 16:35:16 +0900

    fix: remove unused import

--- feature/sponsors/src/commonMain/kotlin/io/github/droidkaigi/confsched/sponsors/component/SponsorItem.kt
+++ feature/sponsors/src/commonMain/kotlin/io/github/droidkaigi/confsched/sponsors/component/SponsorItem.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
