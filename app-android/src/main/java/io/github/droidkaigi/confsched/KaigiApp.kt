70a4aa7cc7e585957f09cfc53aae14cc7d46bb43
kuwa <h.kuwabara1103@gmail.com>
2024-08-18 22:37:35 +0900

    Change navigation target to dark mode launcher activity

--- app-android/src/main/java/io/github/droidkaigi/confsched/KaigiApp.kt
+++ app-android/src/main/java/io/github/droidkaigi/confsched/KaigiApp.kt
@@ -39,7 +39,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.window.layout.DisplayFeature
 import co.touchlab.kermit.Logger
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import io.github.droidkaigi.confsched.about.aboutScreen
 import io.github.droidkaigi.confsched.about.aboutScreenRoute
 import io.github.droidkaigi.confsched.about.navigateAboutScreen
@@ -322,7 +321,7 @@ private class ExternalNavController(
     }
 
     fun navigateToLicenseScreen() {
-        context.startActivity(Intent(context, OssLicensesMenuActivity::class.java))
+        context.startActivity(Intent(context, OssLicensesDarkModeLauncherActivity::class.java))
     }
 
     fun onShareClick(timetableItem: TimetableItem) {
