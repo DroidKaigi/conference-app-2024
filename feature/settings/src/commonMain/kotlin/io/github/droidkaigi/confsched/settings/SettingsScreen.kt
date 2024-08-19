aaf0b51f4ede3c114ef71790842d4da6f37a3bd0
todayama_r <13657682+Corvus400@users.noreply.github.com>
2024-08-19 12:01:45 +0900

    :recycle: Implemented the ability to change fonts on the settings screen.

--- feature/settings/src/commonMain/kotlin/io/github/droidkaigi/confsched/settings/SettingsScreen.kt
+++ feature/settings/src/commonMain/kotlin/io/github/droidkaigi/confsched/settings/SettingsScreen.kt
@@ -1,5 +1,8 @@
 package io.github.droidkaigi.confsched.settings
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -9,16 +12,21 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import conference_app_2024.feature.settings.generated.resources.settings_title
 import io.github.droidkaigi.confsched.compose.rememberEventEmitter
 import io.github.droidkaigi.confsched.designsystem.theme.KaigiTheme
+import io.github.droidkaigi.confsched.model.FontFamily
+import io.github.droidkaigi.confsched.settings.section.accessibility
 import io.github.droidkaigi.confsched.ui.SnackbarMessageEffect
 import io.github.droidkaigi.confsched.ui.UserMessageStateHolder
 import io.github.droidkaigi.confsched.ui.UserMessageStateHolderImpl
 import io.github.droidkaigi.confsched.ui.component.AnimatedLargeTopAppBar
 import io.github.droidkaigi.confsched.ui.handleOnClickIfNotNavigating
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 const val settingsScreenRoute = "settings"
@@ -41,6 +49,7 @@ fun NavGraphBuilder.settingsScreens(
 }
 
 data class SettingsUiState(
+    val useFontFamily: FontFamily?,
     val userMessageStateHolder: UserMessageStateHolder,
 )
 
@@ -63,6 +72,7 @@ fun SettingsScreen(
         uiState = uiState,
         snackbarHostState = snackbarHostState,
         onBackClick = onNavigationIconClick,
+        onSelectUseFontFamily = { eventEmitter.tryEmit(SettingsScreenEvent.SelectUseFontFamily(it)) },
         modifier = modifier,
         isTopAppBarHidden = isTopAppBarHidden,
     )
@@ -71,11 +81,10 @@ fun SettingsScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(
-    // TODO Remove Suppress
-    @Suppress("UnusedParameter")
     uiState: SettingsUiState,
     snackbarHostState: SnackbarHostState,
     onBackClick: () -> Unit,
+    onSelectUseFontFamily: (FontFamily) -> Unit,
     isTopAppBarHidden: Boolean,
     modifier: Modifier = Modifier,
 ) {
@@ -85,13 +94,15 @@ fun SettingsScreen(
         } else {
             null
         }
+    val lazyListState = rememberLazyListState()
+
     Scaffold(
         modifier = modifier,
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         topBar = {
             if (!isTopAppBarHidden) {
                 AnimatedLargeTopAppBar(
-                    title = "Settings",
+                    title = stringResource(SettingsRes.string.settings_title),
                     onBackClick = onBackClick,
                     scrollBehavior = scrollBehavior,
                     navIconContentDescription = "Back",
@@ -99,7 +110,24 @@ fun SettingsScreen(
             }
         },
     ) { padding ->
-        // TODO
+        LazyColumn(
+            Modifier
+                .fillMaxSize()
+                .let {
+                    if (scrollBehavior != null) {
+                        it.nestedScroll(scrollBehavior.nestedScrollConnection)
+                    } else {
+                        it
+                    }
+                },
+            contentPadding = padding,
+            state = lazyListState,
+        ) {
+            accessibility(
+                uiState = uiState,
+                onSelectUseFontFamily = onSelectUseFontFamily
+            )
+        }
     }
 }
 
@@ -110,10 +138,12 @@ fun SettingsScreenPreview() {
         Surface {
             SettingsScreen(
                 uiState = SettingsUiState(
+                    useFontFamily = FontFamily.DotGothic16Regular,
                     userMessageStateHolder = UserMessageStateHolderImpl(),
                 ),
                 snackbarHostState = SnackbarHostState(),
                 onBackClick = {},
+                onSelectUseFontFamily = {},
                 isTopAppBarHidden = false,
             )
         }
