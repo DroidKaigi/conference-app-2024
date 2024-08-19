1a65f025cfe249498ff17c8a86d7d661f2396e6f
rey <ku.k669500@gmail.com>
2024-08-19 22:26:47 +0900

    Show QrCode

--- feature/profilecard/src/commonMain/kotlin/io/github/droidkaigi/confsched/profilecard/component/FlipCard.kt
+++ feature/profilecard/src/commonMain/kotlin/io/github/droidkaigi/confsched/profilecard/component/FlipCard.kt
@@ -51,7 +51,6 @@ import conference_app_2024.feature.profilecard.generated.resources.card_front_or
 import conference_app_2024.feature.profilecard.generated.resources.card_front_pink
 import conference_app_2024.feature.profilecard.generated.resources.card_front_white
 import conference_app_2024.feature.profilecard.generated.resources.card_front_yellow
-import conference_app_2024.feature.profilecard.generated.resources.icon_qr
 import io.github.droidkaigi.confsched.designsystem.theme.KaigiTheme
 import io.github.droidkaigi.confsched.designsystem.theme.LocalProfileCardTheme
 import io.github.droidkaigi.confsched.designsystem.theme.ProvideProfileCardTheme
@@ -129,7 +128,7 @@ internal fun FlipCard(
                 .renderToBytes().toImageBitmap()
         }
         if (isBack) { // Back
-            FlipCardBack(uiState)
+            FlipCardBack(uiState, imageBitmap)
         } else { // Front
             FlipCardFront(
                 uiState = uiState,
@@ -204,6 +203,7 @@ private fun FlipCardFront(
 @Composable
 private fun FlipCardBack(
     uiState: Card,
+    bitmap: ImageBitmap,
     modifier: Modifier = Modifier,
 ) {
     val background = when (uiState.cardType) {
@@ -230,7 +230,7 @@ private fun FlipCardBack(
             contentScale = ContentScale.Crop,
         )
         Image(
-            painter = painterResource(ProfileCardRes.drawable.icon_qr),
+            bitmap = bitmap,
             contentDescription = null,
             modifier = Modifier.size(160.dp),
         )
@@ -274,6 +274,7 @@ fun FlipCardBackPreview() {
         ) {
             FlipCardBack(
                 uiState = uiState,
+                bitmap = QRCode.ofCircles().build(uiState.link).renderToBytes().toImageBitmap(),
             )
         }
     }
