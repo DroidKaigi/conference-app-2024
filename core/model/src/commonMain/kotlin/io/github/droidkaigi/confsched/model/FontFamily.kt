87eb5543bb4f3fdba38d647d317d2741b5fcb474
todayama_r <13657682+Corvus400@users.noreply.github.com>
2024-08-19 11:57:09 +0900

    :sparkles: Created an enum class to handle FontFamily.

---
+++ core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched/model/FontFamily.kt
@@ -0,0 +1,15 @@
+package io.github.droidkaigi.confsched.model
+
+public enum class FontFamily(
+    val displayName: String,
+    val fileName: String,
+) {
+    DotGothic16Regular(
+        displayName = "DotGothic",
+        fileName = "dot_gothic16_regular",
+    ),
+    NotoSansJPRegular(
+        displayName = "NotoSans",
+        fileName = "noto_sans_jp_regular",
+    ),;
+}
