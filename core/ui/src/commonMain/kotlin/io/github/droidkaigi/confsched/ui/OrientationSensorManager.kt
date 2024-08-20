1f97c8c3ab6b67e5e52a11f85688cdff7bed1174
shunm-999 <shunmiyazaki7812@gmail.com>
2024-08-20 23:11:02 +0900

    #210 create Multiplatform SensorManager

---
+++ core/ui/src/commonMain/kotlin/io/github/droidkaigi/confsched/ui/OrientationSensorManager.kt
@@ -0,0 +1,20 @@
+package io.github.droidkaigi.confsched.ui
+
+import androidx.compose.runtime.Composable
+
+data class Orientation(
+    val azimuth: Float,
+    val pitch: Float,
+    val roll: Float,
+)
+
+internal interface OrientationSensorManager {
+    val onOrientationChanged: (Orientation) -> Unit
+    fun start()
+    fun stop()
+}
+
+@Composable
+internal expect fun getOrientationSensorManager(
+    onOrientationChanged: (Orientation) -> Unit,
+): OrientationSensorManager
