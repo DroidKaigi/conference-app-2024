e3d03081ac8bd41cb045dc1f3a1672b479d2c053
Ichinose Yoshihiro <maruisansmai@gmail.com>
2024-08-22 03:17:33 +0900

    feat: Create a Composable function that converts both sides of a card into ImageBitmaps

--- feature/profilecard/src/commonMain/kotlin/io/github/droidkaigi/confsched/profilecard/component/FlipCard.kt
+++ feature/profilecard/src/commonMain/kotlin/io/github/droidkaigi/confsched/profilecard/component/FlipCard.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -28,10 +30,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.layer.drawLayer
+import androidx.compose.ui.graphics.rememberGraphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.SpanStyle
@@ -135,6 +140,76 @@ internal fun FlipCard(
                 profileImage = profileImage,
             )
         }
+    }
+}
+
+@Composable
+internal fun CapturableCard(
+    uiState: Card,
+    onCaptured: (ImageBitmap, ImageBitmap) -> Unit,
+) {
+    val graphicsLayerFront = rememberGraphicsLayer()
+    val graphicsLayerBack = rememberGraphicsLayer()
+    var frontImage: ImageBitmap? by remember { mutableStateOf(null) }
+    var backImage: ImageBitmap? by remember { mutableStateOf(null) }
+    val profileImage = remember { uiState.image.decodeBase64Bytes().toImageBitmap() }
+    val imageBitmap = remember {
+        QRCode.ofCircles()
+            .build(uiState.link)
+            .renderToBytes().toImageBitmap()
+    }
+
+    LaunchedEffect(Unit) {
+        delay(300)
+        frontImage = graphicsLayerFront.toImageBitmap()
+        backImage = graphicsLayerBack.toImageBitmap()
+        onCaptured(graphicsLayerFront.toImageBitmap(), graphicsLayerBack.toImageBitmap())
+    }
+
+    Box(
+        modifier = Modifier
+            .drawWithContent {
+                graphicsLayerFront.record {
+                    this@drawWithContent.drawContent()
+                }
+                drawLayer(graphicsLayerFront)
+            }
+    ) {
+        FlipCardFront(
+            uiState,
+            profileImage = profileImage,
+            modifier = Modifier
+                .size(width = 300.dp, height = 380.dp)
+                .border(
+                    3.dp,
+                    Color.Black,
+                    RoundedCornerShape(8.dp)
+                )
+        )
+    }
+    Box(
+        modifier = Modifier
+            .drawWithContent {
+                graphicsLayerBack.record {
+                    this@drawWithContent.drawContent()
+                }
+                drawLayer(graphicsLayerBack)
+            }
+    ) {
+        FlipCardBack(
+            uiState,
+            imageBitmap,
+            modifier = Modifier
+                .size(width = 300.dp, height = 380.dp)
+                .border(
+                    3.dp,
+                    Color.Black,
+                    RoundedCornerShape(8.dp)
+                )
+                .graphicsLayer {
+                    rotationY = 180f
+                }
+        )
     }
 }
 
