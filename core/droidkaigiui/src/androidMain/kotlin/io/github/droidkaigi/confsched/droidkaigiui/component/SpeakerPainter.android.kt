10ff06c5e648b2688dac31ac1c530bd15a4b8f76
todayama_r <13657682+Corvus400@users.noreply.github.com>
2024-08-29 05:46:08 +0900

    :sparkles: We have added a SpakerPainter method with a placeholder for Android, and iOS will be displayed as existing.

---
+++ core/droidkaigiui/src/androidMain/kotlin/io/github/droidkaigi/confsched/droidkaigiui/component/SpeakerPainter.android.kt
@@ -0,0 +1,19 @@
+package io.github.droidkaigi.confsched.droidkaigiui.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import coil3.request.ImageRequest
+import coil3.request.placeholder
+import io.github.droidkaigi.confsched.core.droidkaigiui.R
+import io.github.droidkaigi.confsched.droidkaigiui.rememberAsyncImagePainter
+
+@Composable
+actual fun speakerPainter(url: String): Painter {
+    return rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(url)
+            .placeholder(R.drawable.icon_place_hoolder)
+            .build()
+    )
+}
