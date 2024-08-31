f2facf29ffc751c8ea8981f1cea4838e970654da
Go Shibata <warahiko@gmail.com>
2024-09-01 02:07:43 +0900

    feat: Define ProfileImage to handle images and their cropping

---
+++ core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched/model/ProfileImage.kt
@@ -0,0 +1,25 @@
+package io.github.droidkaigi.confsched.model
+
+import androidx.compose.ui.unit.IntRect
+
+data class ProfileImage(
+    val bytes: ByteArray,
+) {
+    fun crop(rect: IntRect): ProfileImage {
+        // TODO
+        return this
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as ProfileImage
+
+        return bytes.contentEquals(other.bytes)
+    }
+
+    override fun hashCode(): Int {
+        return bytes.contentHashCode()
+    }
+}
