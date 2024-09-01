aa5c8a703db4460d1ddaf9431c8c268b2bcac51d
todayama_r <13657682+Corvus400@users.noreply.github.com>
2024-09-02 08:24:54 +0900

    :wrench: The problem of flickering icons has been addressed by caching ImageRequests. Every time you scroll, a new ImageRequest instance was created using LocalContext.current. As a result, rememberAsyncImagePainter would create a new Painter each time, causing flickering when scrolling.

--- core/droidkaigiui/src/androidMain/kotlin/io/github/droidkaigi/confsched/droidkaigiui/component/SpeakerPainter.android.kt
+++ core/droidkaigiui/src/androidMain/kotlin/io/github/droidkaigi/confsched/droidkaigiui/component/SpeakerPainter.android.kt
@@ -1,6 +1,7 @@
 package io.github.droidkaigi.confsched.droidkaigiui.component
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import coil3.request.ImageRequest
@@ -10,13 +11,17 @@ import io.github.droidkaigi.confsched.droidkaigiui.rememberAsyncImagePainter
 
 @Composable
 actual fun speakerPainter(url: String): Painter {
-    // TODO Use DrawableResource
-    // https://github.com/DroidKaigi/conference-app-2024/pull/864/files#r1736437080
-    // https://github.com/coil-kt/coil/issues/2077
-    return rememberAsyncImagePainter(
-        model = ImageRequest.Builder(LocalContext.current)
+    val context = LocalContext.current
+
+    val imageRequest = remember(url, context) {
+        // TODO Use DrawableResource
+        // https://github.com/DroidKaigi/conference-app-2024/pull/864/files#r1736437080
+        // https://github.com/coil-kt/coil/issues/2077
+        ImageRequest.Builder(context)
             .data(url)
             .placeholder(R.drawable.icon_place_hoolder)
-            .build(),
-    )
+            .build()
+    }
+
+    return rememberAsyncImagePainter(model = imageRequest)
 }
