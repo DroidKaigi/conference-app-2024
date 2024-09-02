2e880a65f25841da379aa420e865ce9f3cdfc2fc
todayama_r <13657682+Corvus400@users.noreply.github.com>
2024-09-02 11:39:31 +0900

    :wrench: The issue where the bottom navigation overlapped with the Create Profile button and made it difficult to press has been fixed.

--- feature/profilecard/src/iosMain/kotlin/io/github/droidkaigi/confsched/profilecard/ProfileCardViewController.kt
+++ feature/profilecard/src/iosMain/kotlin/io/github/droidkaigi/confsched/profilecard/ProfileCardViewController.kt
@@ -1,5 +1,7 @@
 package io.github.droidkaigi.confsched.profilecard
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.unit.dp
 import io.github.droidkaigi.confsched.compose.EventFlow
 import io.github.droidkaigi.confsched.data.Repositories
 import io.github.droidkaigi.confsched.droidkaigiui.composeViewController
@@ -22,6 +24,10 @@ fun profileCardViewController(
                 imageBitmap.toUiImage() ?: UIImage(),
             )
         },
+        // FIXME This is a workaround. For permanent support, we will get the inset value etc. from the iOS side and respond.
+        contentPadding = PaddingValues(
+            bottom = 30.dp,
+        )
     )
 }
 
