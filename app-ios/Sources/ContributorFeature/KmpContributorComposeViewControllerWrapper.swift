c03af9b4138260f3dacd1129a4ca24a7814ebc99
yimajo <y.imajo@gmail.com>
2024-09-03 22:49:41 +0900

    Change access level

--- app-ios/Sources/ContributorFeature/KmpContributorComposeViewControllerWrapper.swift
+++ app-ios/Sources/ContributorFeature/KmpContributorComposeViewControllerWrapper.swift
@@ -2,26 +2,26 @@ import SwiftUI
 import shared
 import ComposableArchitecture
 
-public struct KmpContributorComposeViewControllerWrapper: UIViewControllerRepresentable {
+struct KmpContributorComposeViewControllerWrapper: UIViewControllerRepresentable {
     public typealias URLString = String
 
-    public let repositories: any Repositories
+    let repositories: any Repositories
     private let onContributorsItemClick: (URLString) -> Void
 
-    public init(onContributorsItemClick: @escaping (URLString) -> Void) {
+    init(onContributorsItemClick: @escaping (URLString) -> Void) {
         @Dependency(\.containerClient) var containerClient
 
         self.repositories = containerClient.repositories()
         self.onContributorsItemClick = onContributorsItemClick
     }
 
-    public func makeUIViewController(context: Context) -> UIViewController {
+    func makeUIViewController(context: Context) -> UIViewController {
         return contributorsViewController(
             repositories: repositories,
             onContributorsItemClick: onContributorsItemClick
         )
     }
 
-    public func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
     }
 }
