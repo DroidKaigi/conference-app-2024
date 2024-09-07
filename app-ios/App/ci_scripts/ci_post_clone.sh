e85f89b56e1af40e870e95aeff0e454c52f85d40
ryoya ito <ry.itto.github@gmail.com>
2024-09-08 00:45:36 +0900

    update ci_scripts

--- app-ios/App/ci_scripts/ci_post_clone.sh
+++ app-ios/App/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-cd "$CI_WORKSPACE"
+cd "$CI_PRIMARY_REPOSITORY_PATH"
 
 brew install openjdk@17
 
 export PATH="/usr/local/opt/openjdk@17/bin:$PATH"
 
-./gradlew :app-ios-shared:assembleSharedReleaseXCFramework
+./gradlew app-ios-shared:assembleSharedDebugXCFramework -Papp.ios.shared.arch=arm64 --no-configuration-cache
