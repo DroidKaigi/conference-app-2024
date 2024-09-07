c83926793b320f668a783b998bd46ffef7a7444d
ryoya ito <ry.itto.github@gmail.com>
2024-09-08 00:45:36 +0900

    update ci_scripts

--- app-ios/App/ci_scripts/ci_pre_xcodebuild.sh
+++ app-ios/App/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-cd "$CI_WORKSPACE"
+cd "$CI_PRIMARY_REPOSITORY_PATH"
 
 # Enalbe skip plugin validation
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+
+SOURCE_PACKAGES_PATH="$CI_PRIMARY_REPOSITORY_PATH/app-ios/SourcePackages"
+xcodebuild -resolvePackageDependencies -project $CI_PROJECT_FILE_PATH -scheme DroidKaigi2024App -derivedDataPath $CI_DERIVED_DATA_PATH -clonedSourcePackagesDirPath $SOURCE_PACKAGES_PATH
 
 # workaround for xcode cloud
 brew install mint
@@ -11,10 +15,8 @@ brew install mint
 mint install cybozu/LicenseList
 mint install SwiftGen/SwiftGen
 
-SOURCE_PACKAGES_PATH="$CI_WORKSPACE/app-ios/SourcePackages"
-xcodebuild -resolvePackageDependencies -project $CI_PROJECT_FILE_PATH -scheme DroidKaigi2024App -derivedDataPath $CI_DERIVED_DATA_PATH -clonedSourcePackagesDirPath $SOURCE_PACKAGES_PATH
+mint run LicenseList "$CI_PRIMARY_REPOSITORY_PATH/app-ios/Sources/App/Resources" $SOURCE_PACKAGES_PATH
 
-mint run LicenseList "$CI_WORKSPACE/app-ios/Sources/About/Resources" $SOURCE_PACKAGES_PATH
-
-export DERIVED_SOURCES_DIR="$CI_WORKSPACE/app-ios/Sources/Theme/"
-mint run SwiftGen --config "$CI_WORKSPACE/app-ios/Sources/Theme/swiftgen.yml"
+# generate code
+export DERIVED_SOURCES_DIR="$CI_PRIMARY_REPOSITORY_PATH/app-ios/Sources/Theme/"
+mint run SwiftGen --config "$CI_PRIMARY_REPOSITORY_PATH/app-ios/Sources/Theme/swiftgen.yml"
