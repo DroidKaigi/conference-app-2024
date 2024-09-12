8de163e7c5ef4332baa642aafbdaabb869f94104
yamaton <yamaton@st.inc>
2024-09-12 22:57:17 +0900

    Replaced hex color codes with themes.

--- feature/main/src/commonMain/kotlin/io/github/droidkaigi/confsched/main/section/GlassLikeBottomNavigation.kt
+++ feature/main/src/commonMain/kotlin/io/github/droidkaigi/confsched/main/section/GlassLikeBottomNavigation.kt
@@ -59,6 +59,7 @@ import androidx.compose.ui.unit.sp
 import dev.chrisbanes.haze.HazeState
 import dev.chrisbanes.haze.hazeChild
 import io.github.droidkaigi.confsched.designsystem.theme.KaigiTheme
+import io.github.droidkaigi.confsched.designsystem.theme.primaryFixed
 import io.github.droidkaigi.confsched.droidkaigiui.animation.onGloballyPositionedWithFavoriteAnimationScope
 import io.github.droidkaigi.confsched.droidkaigiui.useIf
 import io.github.droidkaigi.confsched.main.MainScreenTab
@@ -115,8 +116,7 @@ fun GlassLikeBottomNavigation(
         )
 
         val animatedColor by animateColorAsState(
-            // FIXME: apply theme
-            targetValue = Color(0xFF67FF8D),
+            targetValue = MaterialTheme.colorScheme.primaryFixed,
             label = "animatedColor",
             animationSpec =
             spring(
